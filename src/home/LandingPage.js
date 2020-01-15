4b0e6dbfaa0c3a65d6e480a3a1ee08f0b42a13bc
ladleo <rwelamila.jr@gmail.com>
2020-01-15 11:27:36 +0300

    create about me

--- src/home/LandingPage.js
+++ src/home/LandingPage.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import '../App.css'
 import me from '../assets/me.png'
+import Button from "@material-ui/core/Button";
 
-// import Button from 'react-bootstrap/button'
-
-function Home() {
-    return (
-        <div className="home">
-            <div className={'left auto'}>
-                <h3 className={'lead uppercase'}>i am denis t. rwelamila</h3>
-                <h1 className={'capitalize title'}>software developer</h1>
-                {/*<Button/>*/}
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="home">
+                <div className={'left auto'}>
+                    <h3 className={'lead uppercase'}>i am denis t. rwelamila</h3>
+                    <h1 className={'capitalize title'}>software developer</h1>
+                    <Button variant={'contained'} color={'primary'} size={'large'}
+                            href={"mailto:rwelamila.jr@gmail.com"}>contact me</Button>
+                </div>
+                <div className={'right auto'}>
+                    <img className="me" src={me} alt="Denis Rwelamila"/>
+                </div>
             </div>
-            <div className={'right auto'}>
-                <img className="me" src={me} alt="Denis Rwelamila"/>
-                {/*<p className="intro-text">Hey! <code>LADLEO</code> here!</p>*/}
-            </div>
-        </div>
 
-    )
+        );
+
+    }
 }
-
 export default Home;
--- src/portfolio/WelcomeNote.js
+++ src/portfolio/WelcomeNote.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../App.css'
 
-function WelcomeNote() {
-    return (
-        <div className={'home'}>
-            <p>This is a text describing me as a dev guy man.</p>
-        </div>
-    );
-}
+class WelcomeNote extends React.Component {
+    experienceInYears() {
+        let date = new Date();
+        let time = date.getFullYear() - 2013;
+        return time + " years";
+    }
 
+
+    render() {
+        return (
+            <div className={'home'}>
+                <div className={"auto"}>
+                    <h1 className={'title'}>About Me</h1>
+                    <h3>I have been actively doing professional software development for
+                        about <code>{this.experienceInYears()}</code></h3>
+                    <h3>In all that time i have worked mostly on <code>Web Applications</code> and <code>Phone
+                        Applications</code></h3>
+                    <h3>As much as I want to say I am proficient in #java, #pyhthon, #php, #react.... I will refrain and
+                        say I handle <code>Backend Development</code> just as good as I handle <code>FrontEnd
+                            Development</code></h3>
+                </div>
+            </div>
+        );
+    }
+}
 export default WelcomeNote;
