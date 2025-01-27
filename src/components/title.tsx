f7d67310ffb320f5035076787713b6bc27c814bf
rwelamila-denis <denis@flowteller.com>
2025-01-27 12:42:03 +0300

    title adjustments for small screens

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -6,9 +6,9 @@ export default function Title({
     data: PageTitle
 }) {
     return (
-        <div className="title-with-background p-10">
-        <h2 className="font-bold text-xl md:text-2xl">{data.title}</h2>
-        <p className="font-bold text-4xl md:text-6xl">{data.subtitle}</p>
+        <div className="title-with-background p-5 md:p-10">
+        <h2 className="font-bold text-base md:text-2xl">{data.title}</h2>
+        <p className="font-bold text-2xl md:text-6xl">{data.subtitle}</p>
       </div>
     );
 }
